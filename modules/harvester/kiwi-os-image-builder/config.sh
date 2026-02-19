d405bfef3f89da5aeee765672c7e177e84ed9ef1
Giovanni Lo Vecchio <glovecchio@Mac.homenet.telecomitalia.it>
2026-02-19 09:31:08 +0100

    Fix

--- modules/harvester/kiwi-os-image-builder/config.sh
+++ modules/harvester/kiwi-os-image-builder/config.sh
@@ -9,3 +9,4 @@ systemctl enable cloud-config
 systemctl enable cloud-final
 systemctl enable google-guest-agent
 systemctl enable google-osconfig-agent
+systemctl enable google-startup-scripts.service
