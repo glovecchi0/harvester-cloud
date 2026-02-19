8e119b99ecd19edab4401c8d256441cfc57086fa
Giovanni Lo Vecchio <glovecchio@Mac.homenet.telecomitalia.it>
2026-02-19 09:03:46 +0100

    Fix

--- modules/harvester/kiwi-os-image-builder/config.sh
+++ modules/harvester/kiwi-os-image-builder/config.sh
@@ -7,3 +7,5 @@ systemctl enable cloud-init
 systemctl enable cloud-init-local
 systemctl enable cloud-config
 systemctl enable cloud-final
+systemctl enable google-guest-agent
+systemctl enable google-osconfig-agent
